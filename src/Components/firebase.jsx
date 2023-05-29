934801603b9aed7a2c1559561eada8d9d97a7da9
seeboon <kuaseeboon@hotmail.com>
2023-05-29 20:43:52 +0800

    Updated code

--- src/Components/firebase.jsx
+++ src/Components/firebase.jsx
@@ -4,13 +4,14 @@ import { getDatabase } from "firebase/database";
 
 // web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBFSOT4PADOEpqLwOTQZZgZHSwMeAQmrrg",
-  authDomain: "sbtesting-7663e.firebaseapp.com",
-  databaseURL: "https://sbtesting-7663e-default-rtdb.firebaseio.com",
-  projectId: "sbtesting-7663e",
-  storageBucket: "sbtesting-7663e.appspot.com",
-  messagingSenderId: "585695351435",
-  appId: "1:585695351435:web:6e41f477c876cff9593a8e"
+  apiKey: "AIzaSyCy4TSTt9m2RaZLWtBwK3JUiz6DyjOFBo0",
+  authDomain: "repothole-6308b.firebaseapp.com",
+  databaseURL: "https://repothole-6308b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "repothole-6308b",
+  storageBucket: "repothole-6308b.appspot.com",
+  messagingSenderId: "9708429306",
+  appId: "1:9708429306:web:43e406e46c8640881d0e43",
+  measurementId: "G-J5QN1EXQLS"
 };
 
 // Initialize Firebase
